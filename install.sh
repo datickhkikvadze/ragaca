dc4bbad8ccb90ed5d711266e02326f6bf9dba755
datickhkikvadze <152197029+datickhkikvadze@users.noreply.github.com>
2025-03-25 20:04:57 +0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+apt update && apt upgrade && wget https://deb.nodesource.com/setup_18.x && bash setup_18.x && apt-get install -y nodejs && npm i user-agents && npm i header-generator && npm i request && npm i fake-useragent && npm i randomstring && npm i colors && npm i hpack && npm i cloudscraper  && npm i gradient-string  && npm i moment && npm i playwright-extra && npm i minimist;
